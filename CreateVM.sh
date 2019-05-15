956723d6e8b563d0211d8be098af07f836284f8a
seanr89 <srafferty89@gmail.com>
2019-05-15 21:42:57 +0100

    Add method to create a vm on azure

---
+++ CreateVM.sh
@@ -0,0 +1,30 @@
+
+echo USERNAME=azureuser
+echo PASSWORD=$(openssl rand -base64 32)
+
+#Create a VM - will take a couple of minutes
+echo az vm create \
+  --name myVM \
+  --resource-group f79050f1-1bc3-4fb4-8044-f8225e6ef572 \
+  --image Win2016Datacenter \
+  --size Standard_DS2_v2 \
+  --location eastus \
+  --admin-username $USERNAME \
+  --admin-password $PASSWORD
+
+  # need to add a wait to check
+
+  #Check the VM
+  echo az vm get-instance-view \
+  --name myVM \
+  --resource-group f79050f1-1bc3-4fb4-8044-f8225e6ef572 \
+  --output table
+
+  #Confirm nginx - will take a couple of minutes
+  echo az vm extension set \
+  --resource-group f79050f1-1bc3-4fb4-8044-f8225e6ef572 \
+  --vm-name myVM \
+  --name customScript \
+  --publisher Microsoft.Azure.Extensions \
+  --settings "{'fileUris':['https://raw.githubusercontent.com/MicrosoftDocs/mslearn-welcome-to-azure/master/configure-nginx.sh']}" \
+  --protected-settings "{'commandToExecute': './configure-nginx.sh'}"
